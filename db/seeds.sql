ac18bda041e6e8b58b4fbe7e71ab3ebd59c7eeaf
Clara Bell <athenaolson@Athenas-MacBook-Pro.local>
2018-11-04 15:55:41 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+### Populate data with 3 starter burgers
+INSERT INTO burgers (burger_name) VALUES ('Hangover Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Truffle Waffle Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
