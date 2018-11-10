1029de86c4099930ab44c800c92f53188a256bf8
Clara Bell <athenaolson@Athenas-MacBook-Pro.local>
2018-11-09 19:45:06 -0500

    commit 2

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,11 @@ INSERT INTO burgers (burger_name) VALUES ('Truffle Waffle Burger');
 INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
 
 INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Brown Cow');
+
+INSERT INTO burgers (burger_name) VALUES ('French Fries');
+
+INSERT INTO burgers (burger_name) VALUES ('Banana Split');
+
+INSERT INTO burgers (burger_name) VALUES ('Black Coffee');
